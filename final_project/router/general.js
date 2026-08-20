2756a207d293ce7f6bc5d7a83dc40c41c368114b
PabloHernandez03 <pablo.hernandez6287@alumnos.udg.mx>
2026-08-20 15:01:05 -0400

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,33 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/register", (req, res) => {
+
+    const { username, password } = req.body;
+
+    // Verificar que ambos datos existan
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username and password are required"
+        });
+    }
+
+    // Verificar si el usuario ya existe
+    if (isValid(username)) {
+        return res.status(409).json({
+            message: "Username already exists"
+        });
+    }
+
+    // Registrar usuario
+    users.push({
+        username: username,
+        password: password
+    });
+
+    return res.status(201).json({
+        message: "User registered successfully"
+    });
 });
 
 // Get the book list available in the shop
@@ -62,8 +86,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    isbn = req.params.isbn;
+    reviewsByISBN = [];
+    
+    const book = books[isbn];
+
+
+    return res.send(book.reviews)
 });
 
 module.exports.general = public_users;
