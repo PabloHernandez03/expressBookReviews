9ebd14be22c070799a4d7d0aad7e3b5ec2e93442
PabloHernandez03 <pablo.hernandez6287@alumnos.udg.mx>
2026-08-20 15:08:51 -0400

    task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,53 +1,61 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+
 const regd_users = express.Router();
+
+const JWT_TOKEN = "veryVerySecure";
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-    return users.some(user => user.username === username)
-}
+const isValid = (username) => {
+    return users.some(user => user.username === username);
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username, password) => {
     return users.some(
-        user => user.username == username &&
-            user.password == password
-    )
-}
+        user =>
+            user.username === username &&
+            user.password === password
+    );
+};
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
+// Only registered users can login
+regd_users.post("/login", (req, res) => {
+
     const { username, password } = req.body;
 
     if (!isValid(username)) {
         return res.status(401).json({
             message: "User not registered"
-        })
+        });
     }
 
     if (!authenticatedUser(username, password)) {
         return res.status(401).json({
             message: "Invalid username or password"
-        })
+        });
     }
 
     const accessToken = jwt.sign(
         { username: username },
         JWT_TOKEN
-    )
+    );
 
     req.session.authorization = accessToken;
 
-    return res.send(200).json({
-        message: "Login succesful"
-    })
+    return res.status(200).json({
+        message: "Login successful"
+    });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    isbn = req.params.isbn;
+
+    book = books[isbn];
+
+    
 });
 
 module.exports.authenticated = regd_users;
